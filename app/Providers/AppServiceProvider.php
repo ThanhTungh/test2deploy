86176db86f537cb5ffcadbd21651ae94be91fd16
ThanhTungh <tungtottinh555@gmail.com>
2024-03-18 22:50:06 +0700

    paginator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Routing\UrlGenerator;
 class AppServiceProvider extends ServiceProvider
@@ -20,7 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
-        Paginator::useBootstrapFive();
+        
         if (env('APP_ENV') == 'production') {
             $url->forceScheme('https');
         }
